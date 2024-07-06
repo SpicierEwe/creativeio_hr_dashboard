419ecac868d25ad762353663bd9ae72e17f06fe9
SpicierEwe <hammadtayyab92@gmail.com>
2024-07-06 23:57:11 +0530

    0.0.1

---
+++ .history/src/components/account-details/account-details_20240706185023.jsx
@@ -0,0 +1,167 @@
+function AccountDetails() {
+  return (
+    <div className="lg:pr-12 pb-9 pt-10 ml-20 xl:ml-56 ">
+      <div className="xl:px-10 ">
+        {/* header */}
+        <div className="flex flex-col xl:flex-row items-start xl:justify-between gap-5 w-full ">
+          <div className="text-start flex flex-col gap-2">
+            <h1 className="text-xl text-accent">
+              <span className="font-semibold">Hello,</span> Welcome back Gerald!
+            </h1>
+          </div>
+          <h2 className="text-[#6c747d]">
+            Home - <span className="text-accent">Dashboard</span>
+          </h2>
+        </div>
+        {/*  */}
+        <div className="text-start mt-20 p-8 bg-white min-h-screen text-[#abafb3] ">
+          <header className="mb-8">
+            <h1 className="text-2xl font-semibold">
+              Drive business impact with{" "}
+              <span className="text-accent">
+                Creativo's Client Success Team
+              </span>
+              .
+            </h1>
+          </header>
+
+          <section className="mb-8 text-start ">
+            <p className="mb-4 text-[#abafb3]">
+              At <span className="text-accent">Creativo </span>
+              our dolor sit amet, consetetur sadipscing elitr, sed diam nonu
+              eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+              sed di voluptua. At vero eos et accusam et justo duo dolores et ea
+              rebum. Stet clita kasd gubergren, Creativo sanctus est Lorem ipsum
+              dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing
+              elitr, sed diam nonumy eirmod tempor invidunt{" "}
+            </p>
+
+            <ul className="list-disc pl-5 mb-4 text-start flex flex-col gap-3">
+              <li>
+                <a href="#" className="text-accent">
+                  On-Boarding
+                </a>{" "}
+                - dolor sit amet, consectetur adipiscing elit, sed diam
+                nonummy...
+              </li>
+              <li>
+                <a href="#" className="text-accent">
+                  Webinars
+                </a>{" "}
+                - dolor sit amet, <span className="text-accent">Creativo</span>{" "}
+                adipiscing elit, sed diam nonummy...
+              </li>
+              <li>
+                <a href="#" className="text-accent">
+                  Roundtables
+                </a>{" "}
+                - dolor sit amet, consectetur adipiscing elit, sed diam{" "}
+                <span className="text-accent">Creativo</span>...
+              </li>
+              <li>
+                <a href="#" className="text-accent">
+                  On Going Partnership
+                </a>{" "}
+                - dolor sit amet, consectetur adipiscing elit, sed diam
+                nonummy...
+              </li>
+            </ul>
+            <p className="text-[#abafb3]">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed diam
+              nonummy...
+            </p>
+          </section>
+
+          <section className="flex flex-col justify-around max-w-screen-lg mx-auto  gap-10 mt-20 mb-10">
+            <div className="flex flex-col gap-3 items-center text-center">
+              <img
+                src="/images/account-info/1.svg"
+                alt=""
+                className="w-32 shaodw-md scale-150"
+              />
+              <h2 className="text-xl font-bold mb-2">Live Chat</h2>
+              <p className="text-[#abafb3]">
+                Our support Representatives are available to assist you
+                instantly during business hours!
+              </p>
+              <button className="mt-4 px-4 py-2 bg-accent text-white rounded">
+                Start Chat Now
+              </button>
+            </div>
+
+            <div className="flex flex-col gap-3 items-center text-center">
+              <img
+                src="/images/account-info/2.svg"
+                alt=""
+                className="w-32 shaodw-md scale-150"
+              />
+
+              <h2 className="text-xl font-bold mb-2">Phone</h2>
+              <p className="text-[#abafb3]">
+                Always available during business hours to help! The best way to
+                get you connected
+              </p>
+              <a href="tel:8331235670" className="mt-4 block text-accent">
+                (833)-123-5670
+              </a>
+            </div>
+
+            <div className="flex flex-col gap-3 items-center text-center">
+              <img
+                src="/images/account-info/3.svg"
+                alt=""
+                className="w-32 shaodw-md scale-150"
+              />
+              <h2 className="text-xl font-bold mb-2">Email</h2>
+              <p className="text-[#abafb3]">
+                Need to email us directly? Please allow within 1 business day
+                for response.
+              </p>
+              <a
+                href="mailto:ClientSuccess@creativo.com"
+                className="mt-4 block text-accent"
+              >
+                ClientSuccess@creativo.com
+              </a>
+            </div>
+          </section>
+
+          <section className="p-8 text-center max-w-screen-md mx-auto mt-20">
+            <h2 className="text-xl font-bold mb-4">
+              Let's connect your to our Client Success
+            </h2>
+            <form className="mt-10">
+              <div className="mb-4">
+                <select
+                  id="service"
+                  className="mt-5 block w-full bg-white border-gray-300 rounded-md  border p-5"
+                >
+                  <option>- Select Client Success Service -</option>
+                  {/* Add other options here */}
+                </select>
+              </div>
+              <div className="mb-4 text-start">
+                <label htmlFor="comment" className="block text-gray-700 ">
+                  Question or Comment
+                </label>
+                <textarea
+                  id="comment"
+                  className="mt-3 block w-full border border-gray-300 bg-white rounded-md shadow-sm"
+                  rows="7"
+                ></textarea>
+              </div>
+              <button
+                type="submit"
+                className="px-4 mt-5 py-2 bg-accent text-white rounded"
+              >
+                Submit
+              </button>
+            </form>
+          </section>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AccountDetails;
---
+++ .history/src/components/side_nav/side_nav_20240706235207.jsx
@@ -0,0 +1,362 @@
+import React, { useEffect, useState } from "react";
+import { AiOutlineUserAdd } from "react-icons/ai";
+import { BiMessageError, BiSolidMessage } from "react-icons/bi";
+
+import { CiLock } from "react-icons/ci";
+import { FaBell, FaCheck } from "react-icons/fa";
+import { GiUpgrade } from "react-icons/gi";
+
+import { IoLogOut, IoPerson, IoSearchOutline } from "react-icons/io5";
+import { MdOutlineCardMembership } from "react-icons/md";
+import { RiArrowDropDownLine, RiBillLine } from "react-icons/ri";
+import { RxCross2, RxDashboard, RxPerson } from "react-icons/rx";
+
+import { Link } from "react-router-dom";
+
+const menuItems = [
+  {
+    id: 1,
+    icon: <RxDashboard />,
+
+    label: "Dashboard",
+    notifications: 0,
+    redirect: "/",
+
+    notification: {
+      count: 14,
+      color: "#43e794",
+    },
+  },
+  {
+    id: 2,
+    icon: <RxPerson />,
+    label: "Account Details",
+    notifications: 0,
+    redirect: "/account-details",
+  },
+  {
+    id: 3,
+    icon: <CiLock />,
+    label: "Change Password",
+    notifications: 2,
+    redirect: "/Apps",
+  },
+  {
+    id: 4,
+    icon: <AiOutlineUserAdd />,
+    label: "Add User Access",
+    notifications: 0,
+    redirect: "/Payments",
+
+    notification: {
+      count: 2,
+      color: "#00a2ff",
+    },
+  },
+  {
+    id: 5,
+    icon: <RiBillLine />,
+    label: "Billing History",
+    notifications: 0,
+    redirect: "/Communicate",
+    notification: {
+      count: 6,
+      color: "#ea62b2",
+    },
+  },
+  {
+    id: 6,
+    icon: <MdOutlineCardMembership />,
+    label: "Current Membership",
+    notifications: 0,
+    redirect: "/Reports",
+  },
+  {
+    id: 7,
+    icon: <GiUpgrade />,
+    label: "Upgrade",
+    notifications: 0,
+    redirect: "/Notifications",
+  },
+  {
+    id: 8,
+    icon: <BiMessageError />,
+    label: "Help",
+    notifications: 0,
+    redirect: "/Settings",
+  },
+  {
+    id: 9,
+    icon: <IoLogOut />,
+    label: "Logout",
+    notifications: 0,
+    redirect: "/Logout",
+  },
+];
+
+const messages = [
+  {
+    name: "Henry Nalson",
+    message: "Henry sent you a message.",
+    time: "02",
+  },
+  {
+    name: "Henry Nalson",
+    message: "Henry sent you a message.",
+    time: "02",
+  },
+];
+
+const notifications = [
+  {
+    name: "Henry Nalson",
+    message: "Accpeted your proposal.",
+    time: "20 May 2019, 15:30",
+    isAccepted: true,
+  },
+  {
+    name: "Charles",
+    message: "Rejected our proposal.",
+    time: "20 May 2019, 15:30",
+    isAccepted: false,
+  },
+];
+
+function SideNav() {
+  const [selectedNavIndex, setSelectedNavIndex] = useState(1);
+
+  return (
+    <div className="fixed z-[99999]">
+      {/* top nav */}
+      <TopNavBar />
+
+      {/* side nav */}
+      <ul className="flex flex-col fixed gap-3 h-screen justify-between z-[999] bg-primary">
+        <div className="flex flex-col gap-3  xl:py-5">
+          {/* LOGO images */}
+          <div className="pt-4 pl-3  xl:px-3 xl:pt-0 flex items-center gap-2">
+            {/* mobile */}
+            <img src="/logo/logo.svg" alt="" className="" />
+            <h2 className="hidden font-bold text-2xl xl:block">Creativo</h2>
+          </div>
+
+          {/*  */}
+
+          <div className="mt-9 xl:mt-14 flex flex-col xl:gap-y-3 ">
+            {menuItems.map((item) => {
+              const isSelected = selectedNavIndex === item.id;
+              return (
+                <Link to={item.redirect} key={item.id}>
+                  <li
+                    className={`p-3 border-l-[5px]  text-[##707070] ${
+                      isSelected ? "bg-[#f3f1fa] border-accent " : ""
+                    } text-md cursor-pointer w-full transition-all duration-200 ease-in-out`}
+                    onClick={() => setSelectedNavIndex(item.id)}
+                  >
+                    <div className="flex justify-between gap-1">
+                      <div className="flex gap-4">
+                        {/* item icon */}
+                        <div className={`text-2xl lg:text-xl text-[#707070]`}>
+                          {item.icon}
+                        </div>
+
+                        {/* label */}
+                        <div
+                          className={`hidden lg:block text-sm text-[#6c747d]`}
+                        >
+                          {item.label}
+                        </div>
+                      </div>
+
+                      {/* notifications */}
+
+                      {item.notification !== undefined && (
+                        <div
+                          className="hidden xl:flex rounded-lg font-semibold px-1  text-xs  items-center ml-3.5"
+                          style={{
+                            backgroundColor: item.notification.color,
+                          }}
+                        >
+                          <p className="text-white">
+                            {item.notification.count
+                              .toString()
+                              .padStart(2, "0")}
+                          </p>
+                        </div>
+                      )}
+                    </div>
+                  </li>
+                </Link>
+              );
+            })}
+          </div>
+        </div>
+      </ul>
+    </div>
+  );
+}
+
+export default SideNav;
+
+const TopNavBar = () => {
+  return (
+    <div className="flex justify-end xl:justify-between w-full  bg-primary fixed max-w-screen-2xl p-2 xl:py-[.7rem] py-[1.13rem] border-b-[.5px]  xl:px-10 ">
+      <div className="flex justify-between w-full items-center ml-14 xl:ml-48  ">
+        {/* empty div for layout support */}
+        <div className="flex items-center gap-3 "></div>
+
+        {/* search #*/}
+        <label className="hidden xl:flex input border-gray-300 input-bordered bg-[#f3f6f9] w-[40rem] h-9  items-center gap-2">
+          <input type="text" className="grow" placeholder="Search" />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            className="h-4 w-4 opacity-70 text-[#abafb3]"
+          >
+            <path
+              fillRule="evenodd"
+              d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </label>
+
+        {/* */}
+        <div className="flex items-center">
+          <div className="flex gap-3 items-center">
+            {/* search icon */}
+            <IoSearchOutline size={20} className="text-[#a6adbb]" />
+
+            {/* message icons */}
+
+            {/*  */}
+
+            <details className="dropdown bg-white">
+              <summary className="btn bg-white border-none w-fit hover:bg-accent_light p-0 m-0">
+                <div className="avatar online  placeholder h-7 w-7">
+                  <div className="bg-neutral text-neutral-content w-16 bg-transparent rounded-full">
+                    <span className="text-xl">
+                      <BiSolidMessage className="text-gray-400" />
+                    </span>
+                  </div>
+                </div>
+              </summary>
+              <ul className="menu dropdown-content bg-white  z-[1] w-[20rem] shadow mt-4 right-0">
+                <li className="">
+                  <div className="flex justify-between bg-accent text-white hover:bg-accent">
+                    <div>View messages</div>
+                    <div>View All</div>
+                  </div>
+                  {messages.map((data, index) => {
+                    return (
+                      <div>
+                        <div className="flex gap-5 items-center">
+                          <div className="bg-[#f3f1fa] p-2 rounded-full">
+                            <IoPerson size={22} className="  text-[#abafb3] " />
+                          </div>
+
+                          <div className="flex flex-col gap-1">
+                            <h2 className="text-md font-medium">{data.name}</h2>
+                            <p className="text-sm text-[#abafb3]">
+                              {data.message}
+                            </p>
+                            <p className="text-xs text-[#abafb3]">
+                              {data.time} hours ago
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                    );
+                  })}
+                </li>
+              </ul>
+            </details>
+
+            {/* bell icon */}
+
+            <details className="dropdown bg-white">
+              <summary className="btn bg-white border-none w-fit hover:bg-accent_light p-0 m-0">
+                <div className="avatar online  placeholder h-7 w-7">
+                  <div className="bg-neutral text-neutral-content w-16 bg-accent rounded-full">
+                    <span className="text-xl">
+                      <FaBell className="text-[#9ca3af]" />
+                    </span>
+                  </div>
+                </div>
+              </summary>
+              <ul className="menu dropdown-content bg-white  z-[1] w-[20rem] shadow mt-4 right-0">
+                <li className="">
+                  {notifications.map((data, index) => {
+                    return (
+                      <div>
+                        <div className="flex gap-5 items-center">
+                          <div
+                            className=" p-2 rounded-full"
+                            style={{
+                              backgroundColor: data.isAccepted
+                                ? "#b5dfb7"
+                                : "#ffcbbb",
+                            }}
+                          >
+                            {data.isAccepted ? (
+                              <FaCheck
+                                size={22}
+                                className="  text-[#4caf50] "
+                              />
+                            ) : (
+                              <RxCross2
+                                size={22}
+                                className="  text-[#ff5722] "
+                              />
+                            )}
+                          </div>
+
+                          <div className="flex flex-col gap-1">
+                            <h2 className="text-md font-medium">{data.name}</h2>
+                            <p className="text-sm text-[#abafb3]">
+                              {data.message}
+                            </p>
+                            <p className="text-xs text-[#abafb3]">
+                              {data.time} hours ago
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                    );
+                  })}
+                </li>
+              </ul>
+            </details>
+
+            {/* profile */}
+
+            <details className="dropdown bg-white">
+              <summary className="btn bg-white border-none w-fit hover:bg-accent_light p-0 m-0">
+                {/* profile */}
+                <div className="flex lg:py-2 lg:px-3 ml-3  items-center gap-4  bg-accent_light">
+                  <div className=" w-7 h-7  aspect-square overflow-hidden">
+                    <img
+                      src="https://images.unsplash.com/photo-1500917293891-ef795e70e1f6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                      alt=""
+                      className="aspect-square rounded-full"
+                    />
+                  </div>
+                  <h2 className="hidden font-medium xl:block">Theresa Webb</h2>
+                </div>
+              </summary>
+              <ul className="menu dropdown-content bg-white  z-[1] w-52 p-2 right-0 shadow mt-4">
+                <li>
+                  <a>My Profile</a>
+                </li>
+                <li>
+                  <a>My Wallet</a>
+                </li>
+              </ul>
+            </details>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
